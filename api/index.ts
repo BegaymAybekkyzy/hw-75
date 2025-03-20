1cfbf3e21b1901c30eeb5ac0c84863660ade8028
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-03-20 19:21:02 +0600

    Api: Реализована логика шифровка и расшифровка сообщений с использованием шифра Виженера

---
+++ api/index.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import cors from "cors";
+import {IData} from "./types";
+
+const app = express();
+const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+let data: IData | null = null;
+let encodeMessage = "";
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/encode", async (req, res) => {
+
+    if (req.body.password.trim() === "") {
+        res.status(400).send({error: "Password not entered"});
+        return;
+    }
+
+    if (req.body.message.trim() === "") {
+        res.status(400).send({error: "Message not entered"});
+        return;
+    }
+
+    data = {
+        password: req.body.password,
+        message: req.body.message.toLowerCase(),
+    };
+
+    encodeMessage = await Vigenere.Cipher(data.password).crypt(data.message);
+    res.send({encoded: encodeMessage});
+});
+
+app.post("/decode", async (req, res) => {
+
+    if (!data) {
+        res.status(400).send({error: "Invalid Credentials"});
+       return;
+    }
+
+    if (req.body.password.trim() === "" || req.body.message.trim() === "") {
+        res.status(400).send({error: "Data not entered"});
+        return;
+    }
+
+    if(req.body.message === encodeMessage && req.body.password === data.password) {
+        const decodeMessage = await Vigenere.Decipher(data.password).crypt(req.body.message);
+        res.send({decoded: decodeMessage});
+        return;
+    }
+
+    res.status(400).send({error: "incorrect data"});
+
+});
+
+const run = async () => {
+    app.listen(port, () => {
+        console.log(`Server started on http://localhost:${port}`);
+    })
+};
+
+run().catch(console.error);
